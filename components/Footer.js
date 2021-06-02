eb5f5d2fff1d5def07cb1d79b64a016ad8e19851
ChrisAchinga <achinga.chris@gmail.com>
2021-06-02 13:37:28 +0300

    update Footer and Footer styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,10 @@
-import styles from './Footer.module.css'
+import styles from '@styles/Footer.module.css'
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with{' '}
-        <img src='/netliheart.svg' alt='Netlify Logo' className={styles.logo} />{' '}
-        for you
+        Next and Netlify Starter Template
       </footer>
     </>
   )
