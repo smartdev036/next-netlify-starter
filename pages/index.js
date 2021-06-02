2a628ccfc0b30e67e62782c1e078e00c13d24eff
ChrisAchinga <achinga.chris@gmail.com>
2021-06-02 09:58:34 +0300

    /clean

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className='container'>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Next Netlify Starter!</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
