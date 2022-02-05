46ad29262192b8b5c3f75f10937727d78785ad8f
macbook <achinga.chris@gmail.com>
2022-02-05 21:20:26 +0300

    51/2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '@styles/globals.css'
+import '../styles/globals.css'
 
-function Application({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default Application
+export default MyApp
